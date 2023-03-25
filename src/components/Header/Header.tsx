46fac3cc30c473107f98b81c47d40dff70c5ebcb
Neeraj <neerajv@thirdrocktechkno.com>
2023-03-25 13:39:05 +0530

    Responsive changes

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, { FunctionComponent } from 'react';
 import { Logo } from '../Logo/Logo';
 import { Navigation } from '../Navigation/Navigation';
 
-export const Header = () => {
+export const Header: FunctionComponent = () => {
     return (
-        <header className='flex flex-row justify-between p-5 items-center'>
+        <header className='flex flex-row justify-between p-3 items-center sm:px-10 py-5'>
             <Logo />
             <Navigation />
         </header>
