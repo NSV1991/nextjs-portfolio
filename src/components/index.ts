cacb4f39c9a7d826097c8d761ca4d0b200a7232c
Neeraj <neerajv@thirdrocktechkno.com>
2023-05-02 16:43:17 +0530

    add experience section

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,4 +3,5 @@ export * from './Card/Card';
 export * from './Header/Header';
 export * from './Logo/Logo';
 export * from './Navigation/Navigation';
-export * from './Section/Section';
+export * from './Section/ExperienceSection/ExperienceSection';
+export * from './Section/BlogSection/BlogSection';
