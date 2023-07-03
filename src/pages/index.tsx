bd43a9e58269e9a36610a2c2c6318ad0117a7918
Neeraj <neerajv@thirdrocktechkno.com>
2023-07-03 19:18:52 +0530

    add carousel component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
+import { FeaturedSection } from '@components/Section/FeaturedSection/FeaturedSection';
 import { Banner } from '@components/index';
 
 export default function Home() {
     return (
         <>
             <Banner />
+            <FeaturedSection sectionName='Blogs' />
         </>
     );
 }
