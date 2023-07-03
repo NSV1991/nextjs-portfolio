3a6a50c307c0c37cf23b8a21e05bfede4b8bd2f1
Neeraj <neerajv@thirdrocktechkno.com>
2023-07-03 16:07:38 +0530

    add favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -60,11 +60,27 @@ export default function App({ Component, pageProps }: AppProps) {
                     content={`https://neerajvageele.com${router.route}`}
                 />
                 <meta property='og:type' content='website' />
-                <meta
-                    property='og:site_name'
-                    content='https://neerajvageele.com'
-                />
+                <meta property='og:site_name' content='Neeraj Vageele' />
                 <meta property='og:locale' content='en_US' />
+
+                <link
+                    rel='apple-touch-icon'
+                    sizes='180x180'
+                    href='/apple-touch-icon.png'
+                />
+                <link
+                    rel='icon'
+                    type='image/png'
+                    sizes='32x32'
+                    href='/favicon-32x32.png'
+                />
+                <link
+                    rel='icon'
+                    type='image/png'
+                    sizes='16x16'
+                    href='/favicon-16x16.png'
+                />
+                <link rel='manifest' href='/site.webmanifest' />
             </Head>
             <Layout>
                 <Component {...pageProps} />
